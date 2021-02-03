ad50ede1507bdb0cf87c69cbba694cff8257f231
levkaantonov <levkaantonov@gmail.com>
2021-02-03 22:07:29 +0300

    added empty fragments and nav graph

---
+++ app/src/main/java/levkaantonov/com/study/notice/screens/StartFragment.kt
@@ -0,0 +1,19 @@
+package levkaantonov.com.study.notice.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import levkaantonov.com.study.notice.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
